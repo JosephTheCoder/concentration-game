2d2c455fb74510f2ffa826b46da069224998bf4a
JosephTheCoder <menesesjmmc@gmail.com>
2019-05-03 17:15:25 +0100

    update

--- teacher_code/server.c
+++ teacher_code/server.c
@@ -57,9 +57,8 @@ void send_state_board(int fd)
     char color[11] = {'\0'};
     int sent_cell = 0;
 
-    for (i = 0; i < dim*dim; i++)
+    for (i = 0; i < dim * dim; i++)
     {
-        // in case of an ongoing game
         if (board[i].color[0] != 107 && board[1].color[2] != 200 && board[3].color[3] != 100)
         {
             memset(buffer, 0, BUFFER_SIZE);
@@ -81,13 +80,13 @@ void send_state_board(int fd)
 
             printf("buffer: %s\n", buffer);
             write(fd, buffer, sizeof(buffer));
-            
+
             sent_cell = 1;
         }
     }
 
     // in case of an empty board
-    if(sent_cell == 0)
+    if (sent_cell == 0)
     {
         strcpy(buffer, "empty_board");
         write(fd, buffer, sizeof(buffer));
@@ -206,52 +205,50 @@ void main(int argc, char *argv[])
     // ---- Main loop ----
     while (1)
     {
-        if (nr_players < 2)
+        // Waiting for players
+        new_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &size_addr);
+        if (new_fd == -1)
         {
-            // Waiting for players
-            new_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &size_addr);
-            if (new_fd == -1)
-            {
-                perror("accept");
-                exit(-1);
-            }
-
-            nr_players++;
-
-            printf("Player %d connected!\n", nr_players);
-
-            color = random_color();
-
-            if (nr_players == 1) // in the case of the 1st player we also have to allocate the list!
-            {
-                players_list_head = malloc(sizeof(player_t));
-                if (players_list_head == NULL)
-                    exit(1);
-
-                players_list_head->number = nr_players;
-                players_list_head->fd = new_fd;
-                players_list_head->color[0] = color[0];
-                players_list_head->color[1] = color[1];
-                players_list_head->color[2] = color[2];
-                players_list_head->next = NULL;
-            }
-
-            else // 2nd player case -> push to existing list
-            {
-                push_to_list(players_list_head, color, new_fd);
-            }
-
-            memset(buffer, 0, BUFFER_SIZE); //erase buffer before inserting data
-            sprintf(buffer, "%d", dim);
-            write(new_fd, buffer, sizeof(buffer));
-
-            memset(buffer, 0, BUFFER_SIZE);
-            sprintf(buffer, "%d/%d/%d", color[0], color[1], color[2]);
-            write(new_fd, buffer, sizeof(buffer));
-
-            if(nr_players == 2)
-                send_state = 1;
+            perror("accept");
+            exit(-1);
         }
+
+        nr_players++;
+
+        printf("Player %d connected!\n", nr_players);
+
+        color = random_color();
+
+        if (nr_players == 1) // in the case of the 1st player we also have to allocate the list!
+        {
+            players_list_head = malloc(sizeof(player_t));
+            if (players_list_head == NULL)
+                exit(1);
+
+            players_list_head->number = nr_players;
+            players_list_head->fd = new_fd;
+            players_list_head->color[0] = color[0];
+            players_list_head->color[1] = color[1];
+            players_list_head->color[2] = color[2];
+            players_list_head->next = NULL;
+        }
+
+        else // 2nd player case -> push to existing list
+        {
+            push_to_list(players_list_head, color, new_fd);
+        }
+
+        memset(buffer, 0, BUFFER_SIZE); //erase buffer before inserting data
+        sprintf(buffer, "%d", dim);
+        write(new_fd, buffer, sizeof(buffer));
+
+        memset(buffer, 0, BUFFER_SIZE);
+        sprintf(buffer, "%d/%d/%d", color[0], color[1], color[2]);
+        write(new_fd, buffer, sizeof(buffer));
+
+        if (nr_players == 2)
+            send_state = 1;
+
 
         if (send_state == 1)
         {
@@ -259,13 +256,14 @@ void main(int argc, char *argv[])
 
             while (current != NULL)
             {
-                printf("fd: %d\n", current->fd);
                 send_state_board(current->fd);
                 current = current->next;
             }
-            
-            send_state = 0;
         }
+
+        //thread to listen the players plays
+        // read x y
+        //board_play(board_x, board_y);
 
         // basta uma thread por jogador(em principio)
         // if (nr_players = 2) // se for o 2º jogador então cria a thread do 1º(que nao podia jogar sozinho) e do 2º
